cce40e4db763848f5a66f065b8df1a81a937a5b0
samejiaf <62033843+samejiaf@users.noreply.github.com>
2020-03-10 18:07:38 -0500

    Update 01-primes.cpp

--- 01-primes.cpp
+++ 01-primes.cpp
@@ -2,23 +2,26 @@
 #include <math>
 #include <conio>
 
-double prime (void);
+float prime (void);
+float sum (void);
 
 void main()
 {
- cout<<endl<<"Cantidad de numeros primos entre 500 y 12100 es "<<prim()<<endl;
+ cout<<endl<<"Cantidad de numeros primos entre 500 y 12100 es "<<prime()<<endl;
+
+ cout<<endl<<"La suma de los numeros primos entre 500 y 12100 es "<<sum()<<endl;
 
  getch();
 }
 
-double prime (void)
+float prime (void)
 {
-
- float p,n=0;
+ float p,n=0,f;
  int i,d,t;
 
  for (p=501;p<=12100;p++)
   {
+   f=0,
    t=p;
    for (i=3;i<=p;i++)
     {
@@ -26,9 +29,34 @@ double prime (void)
 
      if (d==0)
       {
-       n++;
+       f++;
       }
     }
+   if (f==1) n++;
+  }
+
+return(n);
+}
+
+float sum (void)
+{
+ float p,n=0,f;
+ int i,d,t;
+
+ for (p=501;p<=12100;p++)
+  {
+   f=0,
+   t=p;
+   for (i=3;i<=p;i++)
+    {
+     d=t%i;
+
+     if (d==0)
+      {
+       f++;
+      }
+    }
+   if (f==1) n=n+p;
   }
 
 return(n);
