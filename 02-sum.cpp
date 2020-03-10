a7d85d1aca5bc1d956c3ee2f57d2a200c5ee69c6
samejiaf <62033843+samejiaf@users.noreply.github.com>
2020-03-10 18:30:45 -0500

    Create 02-sum.cpp

---
+++ 02-sum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<math>
+#include<conio>
+#include<iomanip>
+
+void main()
+{
+ float N,f,i,t;
+
+ cout<<"--------------------------"<<endl;
+ cout<<"  N"<<setw(13)<<"f(N)"<<endl;
+ cout<<"--------------------------"<<endl;
+
+ //despliega el contenido de un arreglo n en formato tabular
+
+ for (N=1;N<=1000;N++)
+  {
+   t=0;
+
+   for (i=1;i<=N;i++)
+    {
+     f=(1/i);
+     t+=f;
+    }
+   cout<<setw(7)<<N<<setw(13)<<t<<endl;
+  }
+
+ getch();
+}
